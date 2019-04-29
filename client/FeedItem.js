12f2c23f9a6727075a269aff5f0b228010ef2655
Ahyman18 <a.o.hyman@gmail.com>
2019-04-29 18:53:18 -0400

    4.29 1853

--- client/FeedItem.js
+++ client/FeedItem.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 // FeedItem should consist of an image (src contained in the data from the AJAX request)
 class FeedItem extends Component {
-
   render() {
     // put render logic here
+    let arrayOfFeeds = [];
+    this.props.url.forEach((item) => {
+      arrayOfFeeds.push(<div className='feedItem'><img key={item.toString()} src={item}></img></div>)
+    })
     return (
       <div style={styles.container}>
+        {arrayOfFeeds}
       </div>
     );
   }

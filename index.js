7611b37321e6e5b7ffd4229170bc415930652cda
Allison <pratt.allisons@gmail.com>
2021-04-23 13:37:50 -0400

    delete commented out code

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(__dirname)); //serves the index.html
-// app.get('/', (req, res) => {
-//   res.status(200).sendFile(path.resolve(__dirname, './index.html'));
-// })
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 }); //listens on port 3000 -> http://localhost:3000/
